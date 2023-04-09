842451e12e17ad51122d7a9197438c6c8befd667
Darnell Champen <zerosalvation@gmail.com>
2023-04-09 19:19:44 -0400

    - Removed dashboard and switched to alpha for the opening screen.

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -16,38 +16,44 @@ return {
 	-- 		{ 'MunifTanjim/nui.nvim' },
 	-- 	},
 	-- },
+	-- {
+	-- 	'glepnir/dashboard-nvim',
+	-- 	event = 'VimEnter',
+	-- 	config = function()
+	-- 		require('dashboard').setup({
+	-- 			theme = 'hyper',
+	-- 			config = {
+	-- 				week_header = {
+	-- 					enable = true,
+	-- 				},
+	-- 				shortcut = {
+	-- 					{ desc = ' Update', group = '@property', action = 'Lazy update', key = 'u' },
+	-- 					{
+	-- 						icon = ' ',
+	-- 						icon_hl = '@variable',
+	-- 						desc = 'Files',
+	-- 						group = 'Label',
+	-- 						action = 'Telescope find_files',
+	-- 						key = 'f',
+	-- 					},
+	-- 					{
+	-- 						icon_hl = '@variable',
+	-- 						desc = 'Recent Files',
+	-- 						group = 'Label',
+	-- 						action = 'Telescope oldfiles',
+	-- 						key = 'r',
+	-- 					},
+	-- 				},
+	-- 			},
+	-- 		})
+	-- 	end,
+	-- 	dependencies = { { 'nvim-tree/nvim-web-devicons' } }
+	-- },
 	{
-		'glepnir/dashboard-nvim',
-		event = 'VimEnter',
+		"goolord/alpha-nvim",
 		config = function()
-			require('dashboard').setup({
-				theme = 'hyper',
-				config = {
-					week_header = {
-						enable = true,
-					},
-					shortcut = {
-						{ desc = ' Update', group = '@property', action = 'Lazy update', key = 'u' },
-						{
-							icon = ' ',
-							icon_hl = '@variable',
-							desc = 'Files',
-							group = 'Label',
-							action = 'Telescope find_files',
-							key = 'f',
-						},
-						{
-							icon_hl = '@variable',
-							desc = 'Recent Files',
-							group = 'Label',
-							action = 'Telescope oldfiles',
-							key = 'r',
-						},
-					},
-				},
-			})
-		end,
-		dependencies = { { 'nvim-tree/nvim-web-devicons' } }
+			require('alpha').setup(require('alpha.themes.dashboard').config)
+		end
 	},
 	{
 		"folke/trouble.nvim",
